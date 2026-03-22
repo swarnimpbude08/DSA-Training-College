b51f314777a6b6713615355463d94183fb257aff
swarnimpbude08 <24panbudes@rbunagpur.in>
2026-03-23 00:04:06 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,45 @@
+# logic 1
+val = input("eneter any value")
+val = input("eneter any value")
+ans = val + val
+print(ans)
+
+# logic 2
+n = int(input("eneter any value"))
+ans = n % 10
+print(ans)
+
+# logic 3
+num = int(input("eneter any value"))
+n1 = num % 10 
+n2 = num // 10 
+ans = n1 + n2
+print(ans)
+
+# logic 4
+num = int(input("eneter any value"))
+n1 = num % 10 
+num = num // 10 
+n2 = num % 10  
+num = num // 10 
+n3 = num % 10  
+num = num // 10 
+n4 = num % 10 
+num = num // 10 
+n5 = num % 10 
+ans = n1 + n2 + n3 + n4 + n5 
+print(ans)
+
+# logic 5
+num = int(input("eneter any value"))
+n1 = num % 10 
+num = num // 10 
+n2 = num % 10  
+num = num // 10 
+n3 = num % 10  
+num = num // 10 
+n4 = num % 10 
+num = num // 10 
+n5 = num % 10 
+rev_num = n1 * 10000 + n2 * 1000 + n3 * 100 + n4 * 10 + n5 * 1
+print(rev_num)
